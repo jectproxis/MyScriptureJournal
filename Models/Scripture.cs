eb3860584f98f759ee1c7d9e4a025b2245c0bd66
proje <proje@GM-DUNGEON-96>
2023-06-02 00:21:57 -0600

    Add project files.

---
+++ Models/Scripture.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyScriptureJournal.Models
+{
+    public class Scripture
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Book")]
+        [Required]
+        public string? ScriptureBook { get; set; }
+
+        [Display(Name = "Chapter")]
+        [Required]
+        public string? ChapterNumber { get; set; }
+
+        [Display(Name = "Verse")]
+        [Required]
+        public string? VerseNumber { get; set; }
+
+        [Display(Name = "Entry Date")]
+        [DataType(DataType.Date)]
+        public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Scripture")]
+        [Required]
+        public string? ScriptureText { get; set; }
+
+        [Required]
+        public string? Notes { get; set; }
+    }
+}
---
+++ Pages/Scriptures/Index.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.View;
+using MyScriptureJournal.Data;
+using MyScriptureJournal.Models;
+
+namespace MyScriptureJournal.Pages.Scriptures
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyScriptureJournal.Data.MyScriptureJournalContext _context;
+
+        public IndexModel(MyScriptureJournal.Data.MyScriptureJournalContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Scripture> Scripture { get;set; } = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Books { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? Book { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            IQueryable<string> bookQuery = from b in _context.Scripture
+                                           orderby b.ScriptureBook
+                                           select b.ScriptureBook;
+
+            var words = from w in _context.Scripture select w;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                words = words.Where(s => s.Notes.Contains(SearchString));
+            }
+
+            if(!string.IsNullOrEmpty(Book)) 
+            {
+                words = words.Where(x => x.ScriptureBook == Book);
+            }
+
+            Books = new SelectList(await bookQuery.Distinct().ToListAsync());
+            Scripture = await words.ToListAsync();
+        }
+    }
+}
